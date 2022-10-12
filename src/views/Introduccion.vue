c6756fe58f59661733d013a2e8d6255d206a86fc
JPaolaMontilloG <paola.montillog@gmail.com>
2022-10-12 08:39:19 -0500

    Update Introduccion.vue y Tema2.vue
    Se cambian los videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -18,7 +18,7 @@
               
     figure.mb-5
     .video
-      iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/-0W_53bo1jE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p.mb-0 #[br]#[br]Muchos éxitos en este proceso de aprendizaje.
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -15,7 +15,7 @@
 
     figure.mb-5
     .video.mb-5
-      iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/PCGtRimK1Kc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5.fcac25
       .row.m-0.align-items-center.justify-content-between
